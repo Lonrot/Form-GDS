054d30bf8241a0ea8735e23411b18ff624848421
Lonrot <angelschmal.1a@gmail.com>
2025-04-15 22:20:32 +0100

    Remove unused GET mappings from IncomeAndSpendingController
    Eliminated the `index` and `showForm` GET mappings as they are no longer required. This cleanup simplifies the controller and removes redundant code, improving maintainability.

--- src/main/java/web/form/gds/webapp/controller/IncomeAndSpendingController.java
+++ src/main/java/web/form/gds/webapp/controller/IncomeAndSpendingController.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,16 +12,6 @@ import web.form.gds.webapp.model.IncomeAndSpending2024;
 @Controller
 public class IncomeAndSpendingController implements WebMvcConfigurer {
 
-    @GetMapping("/index")
-    public String index(){
-        return "index";
-    }
-
-    @GetMapping("/income-and-spending")
-    public String showForm(Model model){
-        model.addAttribute("IncomeAndSpending2024", new IncomeAndSpending2024());
-        return "IncomeAndSpending";
-    }
 
     @PostMapping("/submit")
     public String submitForm(@Valid @ModelAttribute("IncomeAndSpending2024") IncomeAndSpending2024 incomeAndSpending2024, BindingResult result, Model model) {
