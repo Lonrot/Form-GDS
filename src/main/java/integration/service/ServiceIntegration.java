3f3e8eacebed6ed61493d280929337f13b29a564
Lonrot <angelschmal.1a@gmail.com>
2025-04-19 00:00:53 +0100

    Add input validation and tests for getAvailableARs method
    Added null and format validation for the userID parameter in the `getAvailableARs` method to prevent invalid inputs. Modified `ServiceIntegrationTest` to include test cases for null and invalid userIDs. Ensured proper use of assertions to verify exceptions and messages.

--- src/main/java/integration/service/ServiceIntegration.java
+++ src/main/java/integration/service/ServiceIntegration.java
@@ -3,6 +3,7 @@ package integration.service;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
 import web.form.model.AnnualReturnList;
+import wiremock.org.apache.commons.lang3.StringUtils;
 
 @Service
 public class ServiceIntegration {
@@ -14,6 +15,12 @@ public class ServiceIntegration {
     }
 
     public AnnualReturnList getAvailableARs(String userID){
+        if(userID == null || userID.isEmpty()) {
+            throw new IllegalArgumentException("User ID cannot be null or empty");
+        } else if(!StringUtils.isAlpha(userID)) {
+            throw new IllegalArgumentException("User ID must contains only numbers, ID used: " + userID + " ");
+        }
+
         return this.restClient.get().uri("/api/available-ars/{userID}",userID).retrieve().body(AnnualReturnList.class);
     }
 }
--- src/test/java/integration/controller/ServiceIntegrationTest.java
+++ src/test/java/integration/controller/ServiceIntegrationTest.java
@@ -14,7 +14,7 @@ import web.form.FormGdsApplication;
 import web.form.model.AnnualReturnList;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest(classes = FormGdsApplication.class)
@@ -22,11 +22,12 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 @EnableWireMock
 class ServiceIntegrationTest {
 
+    WireMockServer server;
 
 
     @BeforeEach
     void setUp() {
-        WireMockServer server = new WireMockServer(options().port(8888).withRootDirectory("src/main/resources/wiremock"));
+        server = new WireMockServer(options().port(8888).withRootDirectory("src/main/resources/wiremock"));
         server.start();
     }
 
@@ -35,10 +36,38 @@ class ServiceIntegrationTest {
 
     @Test
     void getAvailableARs() {
-        String userID ="256653";
+        String userID = "256653";
         AnnualReturnList annualReturnList = serviceIntegration.getAvailableARs(userID);
         assertNotNull(annualReturnList, "Error getting ARs");
-
+        server.stop();
     }
+
+    @Test
+    void getAvailableARs_nullUserID() {
+        String userID = null;
+        String expectedMessage = "User ID cannot be null or empty";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            serviceIntegration.getAvailableARs(userID);
+        });
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            serviceIntegration.getAvailableARs(userID);
+        });
+        assertTrue(exception.getMessage().contains(expectedMessage));
+    }
+
+    @Test
+    void getAvailableARs_invalidUserID() {
+        String userID = "256653a";
+        String expectedMessage = "User ID must contains only numbers, ID used: 256653a ";
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            serviceIntegration.getAvailableARs(userID);
+        });
+        assertTrue(exception.getMessage().contains(expectedMessage));
+        assertThrows(IllegalArgumentException.class, () -> {
+            serviceIntegration.getAvailableARs(userID);
+        });
+    }
+
 
 }
