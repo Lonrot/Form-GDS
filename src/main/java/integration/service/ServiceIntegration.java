37dfadf0e1f31fb74769dc2c01b72ca1d4a3834f
Lonrot <angelschmal.1a@gmail.com>
2025-04-19 17:40:21 +0100

    Add logging and enhance AR retrieval logic
    Added logging to `ServiceIntegration` to provide detailed insights into Annual Return retrieval. Enhanced the AR fetching implementation with validations and improved return type. Updated mock data to include the `completed` field for better simulation and testing.

--- src/main/java/integration/service/ServiceIntegration.java
+++ src/main/java/integration/service/ServiceIntegration.java
@@ -1,11 +1,14 @@
 package integration.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
-import web.form.model.AnnualReturnList;
+import web.form.model.AnnualReturn;
+import web.form.model.AnnualReturnArray;
 import wiremock.org.apache.commons.lang3.StringUtils;
 
 @Service
+@Slf4j
 public class ServiceIntegration {
 
     private final RestClient restClient;
@@ -14,13 +17,26 @@ public class ServiceIntegration {
         this.restClient = restClientBuilder.baseUrl("http://localhost:8888").build();
     }
 
-    public AnnualReturnList getAvailableARs(String userID){
-        if(userID == null || userID.isEmpty()) {
+    //I prefer a validator factory instead of this but no time I just want to implement basic checking :D
+    //I just hope god forgive my sins
+    public AnnualReturnArray getAvailableARs(String userID) {
+        if (userID == null || userID.isEmpty()) {
             throw new IllegalArgumentException("User ID cannot be null or empty");
-        } else if(!StringUtils.isNumeric(userID)) {
+        } else if (!StringUtils.isNumeric(userID)) {
             throw new IllegalArgumentException("User ID must contains only numbers, ID used: " + userID + " ");
         }
 
-        return this.restClient.get().uri("/api/available-ars/{userID}",userID).retrieve().body(AnnualReturnList.class);
+        AnnualReturnArray annualReturnArray = this.restClient.get()
+                .uri("/api/available-ars/{userID}", userID)
+                .retrieve()
+                .body(AnnualReturnArray.class);
+
+        assert annualReturnArray != null;
+        log.info("Retrieved a total of {} ARs", annualReturnArray.annualReturns().length);
+        for (AnnualReturn annualReturn : annualReturnArray.annualReturns()) {
+            log.info("AR cycle: {}, start date: {}, end date: {}, completed: {}, next to complete: {}", annualReturn.getARCycle(), annualReturn.getARCycleStartDate(), annualReturn.getARCycleEndDate(), annualReturn.getCompleted(), annualReturn.getNextToComplete());
+        }
+        return annualReturnArray;
     }
 }
+
--- src/main/java/web/form/FormGdsApplication.java
+++ src/main/java/web/form/FormGdsApplication.java
@@ -4,11 +4,13 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 @SpringBootApplication
 @Slf4j
+@ComponentScan(basePackages = {"integration", "web.form"})
 public class FormGdsApplication {
 
 	public static void main(String[] args) {
